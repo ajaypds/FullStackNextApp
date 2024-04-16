81ed272ddace56ce299e07ee0802eadb1427f64e
Ajay Prasad <ajay.prasad@outlook.in>
2024-04-16 14:29:29 +0530

    added zod validation schema

--- src/model/Users.ts
+++ src/model/Users.ts
@@ -65,4 +65,6 @@ const UserSchema: Schema<User> = new Schema({
     
 })
 
-const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema)
+const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", UserSchema);
+
+export default UserModel;
